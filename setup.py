208c7bcc9e7220738c9263f4d34b1d817e324877
eliqosterVZ <eliq@vizible.zone>
2020-01-20 21:37:07 +0200

    change release version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'data_from_mongo',         # How you named your package folder (MyLib)
   packages = ['data_from_mongo'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
+  version = '1.1',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'TYPE YOUR DESCRIPTION HERE',   # Give a short description about your library
   author = 'Tzviya',                   # Type in your name
   author_email = 'tzviya@vizible.zone',      # Type in your E-Mail
   url = 'https://github.com/user/reponame',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/tlangent2/data_from_mongo/archive/0.8.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/tlangent2/data_from_mongo/archive/1.1.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'sshtunnel',
