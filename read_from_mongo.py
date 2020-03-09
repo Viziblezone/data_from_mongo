cf0d0fbdadefefbb80227118f0eac500fcb3ac64
tlangent <tzviya@vizible.zone>
2020-03-09 17:08:57 +0200

    add  num_steps, uniquedistance , avr_speed to walking_session

--- read_from_mongo.py
+++ read_from_mongo.py
@@ -155,13 +155,15 @@ import math
 # {['latitude':1]},'gps_longitude':1 ,'gps_speed':1
 
 def read_VZ_from_mongo(mc,_id):
-    dfjson = pd.DataFrame(mc.db.sensors.find({"_id": ObjectId(_id)}, {"_id": 1, 'gps': 1, 'user_id': 1, 'device_type': 1, "timestamp_local": 1, "createdAt": 1}))
+    dfjson = pd.DataFrame(mc.db.sensors.find({"_id": ObjectId(_id)}, {"_id": 1, 'gps': 1, 'user_id': 1, 'device_type': 1, "timestamp_local": 1, "createdAt": 1, "deltaSteps": 1, "distance": 1}))
+
+#    dfjson = pd.DataFrame(mc.db.sensors.find({"_id": ObjectId(_id)}, {"_id": 1, 'gps': 1, 'user_id': 1, 'device_type': 1, "timestamp_local": 1, "createdAt": 1}))
     if len(dfjson) == 0:
         print("_id {} is empty".format(_id))
         return dfjson
     # find number_of_samples
     vecs = ['gps']
-    singles = ['_id', 'user_id', 'device_type', "timestamp_local", "createdAt"]
+    singles = ['_id', 'user_id', 'device_type', "timestamp_local", "createdAt","deltaSteps", "distance"]
     vecs_dfs = []
     min_ts = np.inf
     max_ts = 0
